7b25ca40c991b03d9f9c1b768a01e6a2de11ddc5
jashanpreSingh <149522544+jashanpreSingh@users.noreply.github.com>
2025-06-27 11:24:11 +0000

    conversion operations and comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33"
 
 
-console.log(typeof score)
+//console.log(typeof score)
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
+//console.log(typeof valueInNumber)
 
 
 // "33"  => 33
@@ -16,7 +16,7 @@ console.log(typeof valueInNumber)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -25,4 +25,34 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+//console.log(typeof stringNumber)
+
+
+
+// ****************************   Operations ***********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " jashan"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log(1 + 2 +"2");
+console.log("1" + 2 +2);
+
